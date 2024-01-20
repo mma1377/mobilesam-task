0c217d98584da945d8b2918739b0f8efa6d7c88e
mma1377 <alijani.mohammadmahdi@gmail.com>
2024-01-20 20:54:33 +0100

    fix /segment-image/ endpoint to return the result image as the response

--- service.py
+++ service.py
@@ -1,5 +1,7 @@
 import os
+import io
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
 from main import segment_everything
 from PIL import Image
 
@@ -9,13 +11,13 @@ app = FastAPI()
 def root():
     return {"Hello": "World"}
 
-app = FastAPI()
-
 @app.post("/segment-image/")
 async def segment_image(file: UploadFile = File(...)):
     image = Image.open(file.file).convert("RGB")
     output_path = "generated/output.png"
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     fig = segment_everything(image);
-    fig.save(output_path)
-    return {"filename": file.filename}
+    buffer = io.BytesIO()
+    fig.save(buffer, format='png')
+    buffer.seek(0)
+    return StreamingResponse(buffer, media_type="image/png")
