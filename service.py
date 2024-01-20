21b4a9438f9d7720cad1d99d97e55f27931106cf
mma1377 <alijani.mohammadmahdi@gmail.com>
2024-01-20 20:14:15 +0100

    create /segment-image/ endpoint to run segment_everything

--- service.py
+++ service.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, File, UploadFile
+from main import segment_everything
+from PIL import Image
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+app = FastAPI()
+
+@app.post("/segment-image/")
+async def segment_image(file: UploadFile = File(...)):
+    image = Image.open(file.file).convert("RGB")
+    output_path = "generated/output.png"
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    fig = segment_everything(image);
+    fig.save(output_path)
+    return {"filename": file.filename}
