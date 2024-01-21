76acd55b565b4744fe2cd895a5e0df4b81b966bc
mma1377 <alijani.mohammadmahdi@gmail.com>
2024-01-21 15:55:10 +0100

    The asynchronous method is implemented to avoid service block, as a result server can server muliple users without being blocked by a request

--- service.py
+++ service.py
@@ -1,22 +1,23 @@
-import os
 import io
-from fastapi import FastAPI, File, UploadFile
+import mimetypes
+import asyncio
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 from main import segment_everything
 from PIL import Image
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"Hello": "World"}
+async def segment_everything_async(image):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, segment_everything, image)
 
 @app.post("/segment-image/")
-async def segment_image(file: UploadFile = File(...)):
-    image = Image.open(file.file).convert("RGB")
-    output_path = "generated/output.png"
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    fig = segment_everything(image);
+async def segment_image(image_file: UploadFile = File(...)):
+    if not image_file.content_type.startswith('image'):
+        raise HTTPException(status_code=400, detail="Wrong input! The input must be image.")
+    image = Image.open(image_file.file).convert("RGB")
+    fig = await segment_everything_async(image)
     buffer = io.BytesIO()
     fig.save(buffer, format='png')
     buffer.seek(0)
